f06218aab67793fc56f5a2cf61a8c825295629a8
AlexaBible <AlexaBible@users.noreply.github.com>
2017-01-15 20:36:01 +0000

    updated license

--- Intents/intents.php
+++ Intents/intents.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ *  This file is part of Alexa Tools.
+ *  
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with Alexa Tools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 $intents = new stdclass();
 $intents->categories = array();
 $intents->categories[0] = new stdclass();
